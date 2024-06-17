a0d9bb6245d755f2d46a7e0c797a934c83f95936
cetvrtak <59363714+cetvrtak@users.noreply.github.com>
2024-06-17 11:12:10 +0200

    Add some styling

--- client/src/Login.js
+++ client/src/Login.js
@@ -16,8 +16,8 @@ const Login = ({ setToken }) => {
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <input type="text" value={login} onChange={(e) => setLogin(e.target.value)} placeholder="Login" required />
+        <form onSubmit={handleSubmit} className='login-form'>
+            <input type="text" value={login} onChange={(e) => setLogin(e.target.value)} placeholder="Username" required />
             <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" required />
             <button type="submit">Login</button>
         </form>
